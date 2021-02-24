83aff6ca699804d2a6f1bcfe5553c7e6fd72c4c0
Scott <scott.provence@yahoo.com>
2021-02-24 09:50:43 -0500

    added context and add and delete functionality

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   List as MUIList,
   ListItem,
@@ -11,34 +11,12 @@ import {
 } from "@material-ui/core";
 import { Delete, MoneyOff } from "@material-ui/icons";
 
+import { ExpenseTrackerContext } from "../../../context/context";
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
-
-  const transactions = [
-    {
-      id: 1,
-      type: "Income",
-      category: "Salary",
-      amount: 50,
-      date: "Tue Feb 23 2021",
-    },
-    {
-      id: 2,
-      type: "Expense",
-      category: "Pets",
-      amount: 20,
-      date: "Sun Feb 21 2021",
-    },
-    {
-      id: 3,
-      type: "Expense",
-      category: "Rent",
-      amount: 100,
-      date: "Sat Feb 20 2021",
-    },
-  ];
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
 
   return (
     <MUIList dense={false} className={classes.list}>
@@ -67,7 +45,11 @@ const List = () => {
               secondary={`$${transaction.amount} - ${transaction.date}`}
             />
             <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete" onclick="">
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => deleteTransaction(transaction.id)}
+              >
                 <Delete />
               </IconButton>
             </ListItemSecondaryAction>
