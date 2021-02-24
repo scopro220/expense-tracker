9f26b1c9cb34dd9d1e492b66ddb0139d2431bd26
Scott <scott.provence@yahoo.com>
2021-02-23 22:18:50 -0500

    Initial commit

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  List as MUIList,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Slide,
+} from "@material-ui/core";
+import { Delete, MoneyOff } from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+const List = () => {
+  const classes = useStyles();
+
+  const transactions = [
+    {
+      id: 1,
+      type: "Income",
+      category: "Salary",
+      amount: 50,
+      date: "Tue Feb 23 2021",
+    },
+    {
+      id: 2,
+      type: "Expense",
+      category: "Pets",
+      amount: 20,
+      date: "Sun Feb 21 2021",
+    },
+    {
+      id: 3,
+      type: "Expense",
+      category: "Rent",
+      amount: 100,
+      date: "Sat Feb 20 2021",
+    },
+  ];
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide
+          direction="down"
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transaction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={
+                  transaction.type === "Income"
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`$${transaction.amount} - ${transaction.date}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onclick="">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  );
+};
+
+export default List;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  Typography,
+  Grid,
+  Divider,
+  CardContent,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+import Form from "./Form/Form";
+import List from "./List/List";
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography variant="h5" align="center">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          InfoCard.............
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
